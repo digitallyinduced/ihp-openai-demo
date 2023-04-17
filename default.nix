800d5a66ad3ae21174b0a47fe3696e68fbfe270e
Marc Scholten <marc@digitallyinduced.com>
2023-04-17 23:38:58 +0200

    adjusted IHP version

--- default.nix
+++ default.nix
@@ -1,7 +1,7 @@
 let
     ihp = builtins.fetchGit {
         url = "https://github.com/digitallyinduced/ihp.git";
-        rev = "4c6cf60d7c2157fc7a0e8b23336f95530dc1184c";
+        ref = "refs/heads/openai";
     };
     haskellEnv = import "${ihp}/NixSupport/default.nix" {
         ihp = ihp;
